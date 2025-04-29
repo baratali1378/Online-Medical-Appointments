d875f5c02d142915806d816ceece28a44c6f40a8
Barat Ali <baratalihassanzada1378@gmail.com>
2025-04-29 17:36:06 +0330

    change saved

--- frontend/src/types/next-auth.d.ts
+++ frontend/src/types/next-auth.d.ts
@@ -1,24 +1,32 @@
-import NextAuth from "next-auth";
+// types/next-auth.d.ts
+import "next-auth";
 
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
       email: string;
+      name?: string;
       role: string;
+      image?: string | null;
     };
   }
 
   interface User {
     id: string;
     email: string;
+    name?: string;
     role: string;
+    image?: string | null;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
     id: string;
+    email?: string;
+    name?: string;
     role: string;
+    image?: string | null;
   }
 }
