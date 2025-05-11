1b6b889c22e5d29a46686e4eaa5199a783b3f2d7
Barat Ali <baratalihassanzada1378@gmail.com>
2025-05-11 14:37:09 +0330

    add: about test added

---
+++ backend/tests/api/about/about.test.js
@@ -0,0 +1,53 @@
+// @ts-ignore
+const { setupStrapi, cleanupStrapi } = require("../../helpers/strapi");
+const request = require("supertest");
+
+// @ts-ignore
+describe("GET localhost:1337/api/about", () => {
+  const testAboutData = {
+    title: "About Our Medical Clinic",
+    seoTitle: "Healthcare Services - About Us",
+    content: "<p>Quality care since 2020</p>",
+  };
+
+  // @ts-ignore
+  beforeAll(async () => {
+    await setupStrapi();
+    // Create test data before all tests
+    // @ts-ignore
+    await strapiInstance.entityService.update("api::about.about", 1, {
+      data: testAboutData,
+    });
+  }, 10000);
+
+  // @ts-ignore
+  afterAll(async () => {
+    await cleanupStrapi();
+  }, 10000);
+
+  // @ts-ignore
+  it("should return about content with 200 status", async () => {
+    // @ts-ignore
+    await request(strapiInstance.server.httpServer)
+      .get("/api/about")
+      .expect(200)
+      .then((response) => {
+        // @ts-ignore
+        expect(response.body.data).toBeDefined();
+        // @ts-ignore
+        expect(response.body.data.attributes.title).toBe(testAboutData.title);
+        // @ts-ignore
+        expect(response.body.data.attributes.content).toBe(
+          testAboutData.content
+        );
+      });
+  });
+
+  // @ts-ignore
+  it("should return proper content-type header", async () => {
+    // @ts-ignore
+    await request(strapiInstance.server.httpServer)
+      .get("/api/about")
+      .expect("Content-Type", /application\/json/);
+  });
+});
