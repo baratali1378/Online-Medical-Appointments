2d797d1a1ea69f308d63d94ddd20e50d3b5eaf4f
Barat Ali <baratalihassanzada1378@gmail.com>
2025-05-12 15:02:46 +0330

    add: change added

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -7,7 +7,6 @@ import {
   Container,
   IconButton,
   Drawer,
-  Box,
   useMediaQuery,
   useTheme,
 } from "@mui/material";
@@ -26,29 +25,6 @@ const Header = () => {
 
   const handleDrawerToggle = () => setMobileOpen(!mobileOpen);
 
-  // Show minimal header while session is loading
-  if (status === "loading") {
-    return (
-      <AppBar position="static" color="default" elevation={1} sx={{ py: 1 }}>
-        <Container maxWidth="lg">
-          <Toolbar disableGutters sx={{ justifyContent: "space-between" }}>
-            <Logo />
-            {!isMobile && (
-              <Box sx={{ display: "flex", gap: 3, alignItems: "center" }}>
-                {NAV_ITEMS.map((item) => (
-                  <Box
-                    key={item.label}
-                    sx={{ width: 80, height: 24, bgcolor: "#f0f0f0" }}
-                  />
-                ))}
-              </Box>
-            )}
-          </Toolbar>
-        </Container>
-      </AppBar>
-    );
-  }
-
   return (
     <AppBar position="static" color="default" elevation={1} sx={{ py: 1 }}>
       <Container maxWidth="lg">
@@ -56,7 +32,11 @@ const Header = () => {
           <Logo />
 
           {!isMobile ? (
-            <DesktopNavigation items={NAV_ITEMS} session={session} />
+            <DesktopNavigation
+              items={NAV_ITEMS}
+              session={session}
+              status={status}
+            />
           ) : (
             <IconButton
               color="inherit"
@@ -83,6 +63,7 @@ const Header = () => {
           items={NAV_ITEMS}
           onClose={handleDrawerToggle}
           session={session}
+          status={status}
         />
       </Drawer>
     </AppBar>
