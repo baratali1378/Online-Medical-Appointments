fcafea7d7e5f4c63d6f20d73206eec106432d983
Barat Ali <baratalihassanzada1378@gmail.com>
2025-05-14 13:28:16 +0330

    add: search added to backend

--- backend/src/api/doctor/routes/doctor.js
+++ backend/src/api/doctor/routes/doctor.js
@@ -1,12 +1,22 @@
 module.exports = {
-    routes: [
-        {
-            method: "POST",
-            path: "/doctors/auth",
-            handler: "auth.login",
-            config: {
-                auth: false, // No authentication required
-            },
-        },
-    ],
+  routes: [
+    {
+      method: "POST",
+      path: "/doctors/auth",
+      handler: "auth.login",
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: "GET",
+      path: "/doctors/search",
+      handler: "doctor.search",
+      config: {
+        policies: [],
+        middlewares: [],
+        auth: false,
+      },
+    },
+  ],
 };
