3a457583b682a555079f09b1a64f095d71e8a411
Barat Ali <baratalihassanzada1378@gmail.com>
2025-05-17 18:22:46 +0330

    add: footer added

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -15,40 +15,10 @@ export interface ContactPhoneNumber extends Struct.ComponentSchema {
   };
 }
 
-export interface LinksSocialLinks extends Struct.ComponentSchema {
-  collectionName: 'components_links_social_links';
-  info: {
-    description: '';
-    displayName: 'Social Links';
-    icon: 'link';
-  };
-  attributes: {
-    icon: Schema.Attribute.Media<'images' | 'files'> &
-      Schema.Attribute.Required;
-    platform: Schema.Attribute.String & Schema.Attribute.Required;
-    url: Schema.Attribute.Text;
-  };
-}
-
-export interface UsefulLinksUsefulLinks extends Struct.ComponentSchema {
-  collectionName: 'components_useful_links_useful_links';
-  info: {
-    description: '';
-    displayName: 'Useful Links';
-    icon: 'link';
-  };
-  attributes: {
-    name: Schema.Attribute.String & Schema.Attribute.Required;
-    url: Schema.Attribute.Text & Schema.Attribute.Required;
-  };
-}
-
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
       'contact.phone-number': ContactPhoneNumber;
-      'links.social-links': LinksSocialLinks;
-      'useful-links.useful-links': UsefulLinksUsefulLinks;
     }
   }
 }
