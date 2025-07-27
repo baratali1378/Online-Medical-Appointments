44ff6286f68bdaa0352659bd1b84206d3a9b5a2a
Barat Ali <baratalihassanzada1378@gmail.com>
2025-07-28 00:51:48 +0330

    change saved

--- backend/src/api/metric/routes/metric.js
+++ backend/src/api/metric/routes/metric.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 /**
  * metric router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::metric.metric');
+module.exports = createCoreRouter("api::metric.metric");
